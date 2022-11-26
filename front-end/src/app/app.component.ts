476dc35409ed7513044d22929fcff44f32413587
GuilhermeMenoci <guimenoci@gmail.com>
2022-11-25 22:10:07 -0300

    integrando o post com backend

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms'
+import { Todo } from './todo';
+import { TodoService} from './todo.service' 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  mensagem: string = 'hello world';
+  form: FormGroup = new FormGroup({
+    description : new FormControl('')
+  })
+
+  constructor(
+    private service: TodoService
+  ){}
+
+  submit(){
+    console.log(this.form.value)
+    const todo: Todo = { ...this.form.value }
+    this.service.salvar(todo).subscribe(savedTodo => console.log(savedTodo))
+  }
+
 }
